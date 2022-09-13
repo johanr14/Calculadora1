754a6fc87c17073f384c76d2588b85f854024ed8
johanr14 <113403334+johanr14@users.noreply.github.com>
2022-09-12 22:34:18 -0400

    Iniciar proyecto

---
+++ script.js
@@ -0,0 +1,26 @@
+function sumar() {
+  var x = parseInt(document.getElementById('num1').value);
+  var y = parseInt(document.getElementById('num2').value);
+  document.getElementById('resultado').innerHTML=x + y;
+}
+
+function restar() {
+  var x = parseInt(document.getElementById('num1').value);
+  var y = parseInt(document.getElementById('num2').value);
+  document.getElementById('resultado').innerHTML=x - y;
+  
+}
+
+function multiplicar() {
+  var x = parseInt(document.getElementById('num1').value);
+  var y = parseInt(document.getElementById('num2').value);
+  document.getElementById('resultado').innerHTML=x * y;
+  
+}
+
+function dividir() {
+  var x = parseInt(document.getElementById('num1').value);
+  var y = parseInt(document.getElementById('num2').value);
+  document.getElementById('resultado').innerHTML=x / y;
+  
+}
